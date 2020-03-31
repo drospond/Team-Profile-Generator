8cd1dc01165d06284fe15110b0ebc35dc2c29145
drospond <david.rospond@gmail.com>
2020-03-31 16:06:09 -0400

    Created Engineer class

--- test/Employee.test.js
+++ test/Employee.test.js
@@ -22,25 +22,25 @@ test("Can set email via constructor argument", () => {
   const e = new Employee("Foo", 1, testValue);
   expect(e.email).toBe(testValue);
 });
-
+//passed
 test("Can get name via getName()", () => {
   const testValue = "Alice";
   const e = new Employee(testValue);
   expect(e.getName()).toBe(testValue);
 });
-
+//passed
 test("Can get id via getId()", () => {
   const testValue = 100;
   const e = new Employee("Foo", testValue);
   expect(e.getId()).toBe(testValue);
 });
-
+//passed
 test("Can get email via getEmail()", () => {
   const testValue = "test@test.com";
   const e = new Employee("Foo", 1, testValue);
   expect(e.getEmail()).toBe(testValue);
 });
-
+//passed
 test("getRole() should return \"Employee\"", () => {
   const testValue = "Employee";
   const e = new Employee("Alice", 1, "test@test.com");
