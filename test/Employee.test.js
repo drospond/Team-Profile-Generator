8cf202d4916a4f192f48e302eb68cc9e072bf703
drospond <david.rospond@gmail.com>
2020-03-31 15:46:52 -0400

    Made employee constructor

--- test/Employee.test.js
+++ test/Employee.test.js
@@ -1,22 +1,22 @@
 const Employee = require("../lib/Employee");
-
+//passed
 test("Can instantiate Employee instance", () => {
   const e = new Employee();
   expect(typeof(e)).toBe("object");
 });
-
+//passed
 test("Can set name via constructor arguments", () => {
   const name = "Alice";
   const e = new Employee(name);
   expect(e.name).toBe(name);
 });
-
+//passed
 test("Can set id via constructor argument", () => {
   const testValue = 100;
   const e = new Employee("Foo", testValue);
   expect(e.id).toBe(testValue);
 });
-
+//passed
 test("Can set email via constructor argument", () => {
   const testValue = "test@test.com";
   const e = new Employee("Foo", 1, testValue);
